b37a39c8385e2042c9dca7075cdd9510997018f3
Dilmurod1992 <dil9295a@gmail.com>
2025-07-11 09:32:07 +0500

    Create strings.py

---
+++ lesson-1/homework/strings.py
@@ -0,0 +1,47 @@
+
+# side = float(input("Enter the side of the square: "))
+
+# perimeter = 4 * side
+
+# area = side * side
+
+# print("Perimeter of the square:", perimeter)
+# print("Area of the square:", area)
+
+
+# import math
+
+# diameter = float(input("Enter the diameter of the circle: "))
+
+# circumference = math.pi * diameter
+
+# print("Circumference (length) of the circle:", circumference)
+
+
+
+# a = float(input("Enter the first number (a): "))
+# b = float(input("Enter the second number (b): "))
+
+
+# mean = (a + b) / 2
+
+# print("Mean of the two numbers:", mean)
+
+
+
+
+
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+sum_ab = a + b
+
+product_ab = a * b
+
+square_a = a ** 2
+square_b = b ** 2
+
+print("Sum of a and b:", sum_ab)
+print("Product of a and b:", product_ab)
+print("Square of a:", square_a)
+print("Square of b:", square_b)
