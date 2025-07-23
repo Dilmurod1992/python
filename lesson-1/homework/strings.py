1da17d105d200a5841a74807c384e05ecd212c98
Dilmurod1992 <dil9295a@gmail.com>
2025-07-23 13:47:57 +0500

    Update strings.py

--- lesson-1/homework/strings.py
+++ lesson-1/homework/strings.py
@@ -1,31 +1,31 @@
 
-# side = float(input("Enter the side of the square: "))
+side = float(input("Enter the side of the square: "))
 
-# perimeter = 4 * side
+perimeter = 4 * side
 
-# area = side * side
+area = side * side
 
-# print("Perimeter of the square:", perimeter)
-# print("Area of the square:", area)
-
-
-# import math
-
-# diameter = float(input("Enter the diameter of the circle: "))
-
-# circumference = math.pi * diameter
-
-# print("Circumference (length) of the circle:", circumference)
+print("Perimeter of the square:", perimeter)
+print("Area of the square:", area)
 
 
+import math
 
-# a = float(input("Enter the first number (a): "))
-# b = float(input("Enter the second number (b): "))
+diameter = float(input("Enter the diameter of the circle: "))
+
+circumference = math.pi * diameter
+
+print("Circumference (length) of the circle:", circumference)
 
 
-# mean = (a + b) / 2
 
-# print("Mean of the two numbers:", mean)
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+
+mean = (a + b) / 2
+
+print("Mean of the two numbers:", mean)
 
 
 
