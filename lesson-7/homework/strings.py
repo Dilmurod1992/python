b9d899780bfb7c2cdb68fd91563ca23dfee46c3a
Dilmurod1992 <dil9295a@gmail.com>
2025-07-24 16:53:26 +0500

    Create strings.py

---
+++ lesson-7/homework/strings.py
@@ -0,0 +1,37 @@
+def is_prime(n):
+    if n < 2:
+        return False  
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False  
+    return True
+
+print(is_prime(4))  
+print(is_prime(7))  
+
+
+
+
+
+def digit_sum(k):
+    return sum(int(digit) for digit in str(k))
+
+
+print(digit_sum(24))   
+print(digit_sum(502))  
+
+
+
+
+
+
+def print_powers_of_two(N):
+    k = 1
+    while (power := 2 ** k) <= N:
+        print(power, end=' ')
+        k += 1
+
+
+
+print_powers_of_two(10)
+
