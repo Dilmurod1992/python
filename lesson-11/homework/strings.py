aac367b91b60d74431fcea3644ab1bbe892ae4f7
Dilmurod1992 <dil9295a@gmail.com>
2025-07-30 14:15:40 +0500

    Create strings.py

---
+++ lesson-11/homework/strings.py
@@ -0,0 +1,36 @@
+from math_operations import substract
+print(substract(17,15))
+
+
+
+from math_operations import add
+print(add(17,15))
+
+
+
+from math_operations import multiply
+print(multiply(17,15))
+
+
+
+from math_operations import devide
+print(devide(100,10))
+
+
+
+from string_utils import reverse_string, count_vowels
+
+print(reverse_string("Dilmurod"))     
+print(count_vowels("Republic of Uzbekistan")) 
+
+
+
+
+
+from geometry.circle import calculate_area, calculate_circumference
+
+r = 5
+print("Area:", calculate_area(r))
+print("Circumference:", calculate_circumference(r))
+
+
